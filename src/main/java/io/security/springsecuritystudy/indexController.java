e5654ce0af4dfc45441fa7ea93bdad20f2ca2df5
sfK <sfk.sjm@gmail.com>
2025-01-09 09:07:41 +0900

    익명 인증 사용자 – anonymous()

--- src/main/java/io/security/springsecuritystudy/indexController.java
+++ src/main/java/io/security/springsecuritystudy/indexController.java
@@ -1,5 +1,9 @@
 package io.security.springsecuritystudy;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,25 @@ public class indexController {
     @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
+        return context.getAuthentication().getName();
     }
 }
