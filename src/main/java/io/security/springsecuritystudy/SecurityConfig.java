c6d11e36ca12b75cb7ad86edf0252603f3325c7d
sfK <sfk.sjm@gmail.com>
2025-02-01 16:54:08 +0900

    세션 고정 보호 - sessionManagement().sessionFixation()

--- src/main/java/io/security/springsecuritystudy/SecurityConfig.java
+++ src/main/java/io/security/springsecuritystudy/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,11 +25,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session
-                        .invalidSessionUrl("/invalidSessionUrl")
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false)
-                        .expiredUrl("/expiredUrl")
-                )
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::newSession))
         ;
 
         return http.build();
