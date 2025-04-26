de2b91756b0e6d7b3bf0a12cce8cac4f4e78b46b
sfK <sfk.sjm@gmail.com>
2025-04-26 13:13:30 +0900

    Rest 인증 보안 및 화면 구성

---
+++ src/main/java/io/security/springsecuritystudy/users/controller/RestLoginController.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritystudy.users.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestLoginController {
+
+    @PostMapping("/api/login")
+    public String restLogin() {
+        return "restLogin";
+    }
+}
