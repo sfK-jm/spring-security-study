0773751321ab0d51abce128d83a68bae80d676bb
sfK <sfk.sjm@gmail.com>
2025-04-27 20:15:47 +0900

    Rest 인증 필터 구현 - RestAuthenticationFilter

---
+++ src/main/java/io/security/springsecuritystudy/security/filters/RestAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package io.security.springsecuritystudy.security.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.security.springsecuritystudy.domain.dto.AccountDto;
+import io.security.springsecuritystudy.security.token.RestAuthenticationToken;
+import io.security.springsecuritystudy.util.WebUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public RestAuthenticationFilter() {
+        super(new AntPathRequestMatcher("/api/login", "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        if (!HttpMethod.POST.name().equals(request.getMethod()) || !WebUtil.isAjax(request)) {
+            throw new IllegalArgumentException("Authentication method is not supported");
+        }
+
+        AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
+        if (!StringUtils.hasText(accountDto.getUsername()) || !StringUtils.hasText(accountDto.getPassword())) {
+            throw new AuthenticationServiceException("Username or Password is not provided");
+        }
+
+        RestAuthenticationToken authenticationToken = new RestAuthenticationToken(accountDto.getUsername(), accountDto.getPassword());
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+}
