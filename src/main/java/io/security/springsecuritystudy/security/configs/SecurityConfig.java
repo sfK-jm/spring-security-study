5e1fe366719f1a2c1e0ae7409a02f50e587c23e8
sfK <sfk.sjm@gmail.com>
2025-04-28 20:29:16 +0900

    RestAuthenticationProvider 구현하기

--- src/main/java/io/security/springsecuritystudy/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritystudy/security/configs/SecurityConfig.java
@@ -27,6 +27,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationProvider restAuthenticationProvider;
     private final AuthenticationSuccessHandler successHandler;
     private final AuthenticationFailureHandler failureHandler;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
@@ -62,6 +63,7 @@ public class SecurityConfig {
     public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
 
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
         AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
         http
